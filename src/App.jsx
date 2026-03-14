f1d83a92a7d4892858ee762c5c87e6ef970be745
EltonLeite100 <prudencia@prudenciaconsultoria.com.br>
2026-03-14 15:36:16 -0300

    style: justify paragraph text across pages

--- src/App.jsx
+++ src/App.jsx
@@ -318,7 +318,7 @@ export default function App() {
   };
 
   return (
-    <div className="font-sans text-gray-800 bg-gray-50 min-h-screen flex flex-col">
+    <div className="font-sans text-gray-800 bg-gray-50 min-h-screen flex flex-col [&_p]:text-justify">
       {/* HEADER / NAVBAR */}
       <nav className="fixed w-full bg-white shadow-md z-50">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
